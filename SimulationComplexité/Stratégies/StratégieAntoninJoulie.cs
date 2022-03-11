267de734d1c9c880b643d3878bbff1b51df30e89
Antonin <antonin.joulie@ynov.com>
2022-03-11 13:49:12 +0100

    Rename

--- SimulationComplexité/Stratégies/StratégieAntoninJoulie.cs
+++ SimulationComplexité/Stratégies/StratégieAntoninJoulie.cs
@@ -2,7 +2,7 @@
 
 namespace SimulationComplexité.Stratégies
 {
-    internal class VotreStratégie : IStratégieQualité
+    internal class StratégieAntoninJoulie : IStratégieQualité
     {
         /// <inheritdoc />
         public uint MontantInvestiEnQualité(uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint scoreProduitActuel, ushort coutDUnDé)
