8e4ce6683bc2626988d036b0490ceba29d602662
Enzo Sandré <atelier@enzosandre.fr>
2022-03-30 11:47:40 +0200

    Matteo LECUIT

---
+++ SimulationComplexité/Stratégies/MatteoLecuit_Production.cs
@@ -0,0 +1,18 @@
+﻿using SimulationComplexité.Simulation.Stratégie;
+
+namespace SimulationComplexité.Stratégies
+{
+    internal class MatteoLecuit_Production : IStratégieQualité
+    {
+        /// <inheritdoc />
+        public uint MontantInvestiEnQualité(uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint scoreProduitActuel, ushort coutDUnDé)
+        {
+            if (valeurProduiteBrute <= 5) return 0;
+            if (complexitéAccidentelleActuelle >= valeurProduiteBrute) return valeurProduiteBrute - 1;
+            return complexitéAccidentelleActuelle /2;
+        }
+
+        /// <inheritdoc />
+        public IStratégieQualité Fork() => new MatteoLecuit_Production();
+    }
+}
