6768094de18379265043b02fe5ee4fe5f372584f
Enzo Sandré <atelier@enzosandre.fr>
2022-03-30 11:53:31 +0200

    Clément GEOFFREY

---
+++ SimulationComplexité/Stratégies/ClermontGeoffrey_Production.cs
@@ -0,0 +1,79 @@
+﻿using SimulationComplexité.Simulation.Stratégie;
+
+namespace SimulationComplexité.Stratégies
+{
+    internal class StratégieClermontGeoffrey : IStratégieQualité
+    {
+        int iterations = 0;
+        /// <inheritdoc />
+        public uint MontantInvestiEnQualité(uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint scoreProduitActuel, ushort coutDUnDé)
+        {
+            uint valeurQualité = 0;
+
+            if (iterations == 0) {
+                valeurQualité = valeurProduiteBrute / 2;
+                iterations++;
+                return valeurQualité;
+            } else {
+                uint valeurInvestissable = 0;
+
+                if (complexitéAccidentelleActuelle > valeurProduiteBrute) {
+                    valeurInvestissable = valeurProduiteBrute;
+                } else {
+                    valeurInvestissable = valeurProduiteBrute - complexitéAccidentelleActuelle;
+                }
+
+                decimal minNouvValeurEntropie;
+
+                if (complexitéAccidentelleActuelle < valeurProduiteBrute) {
+                    minNouvValeurEntropie = Convert.ToDecimal(scoreProduitActuel + complexitéAccidentelleActuelle + 2 * valeurProduiteBrute - 4 * ((valeurProduiteBrute/2) + complexitéAccidentelleActuelle)) / coutDUnDé;
+                } else {
+                    minNouvValeurEntropie = Convert.ToDecimal((scoreProduitActuel + complexitéAccidentelleActuelle - 2 * valeurProduiteBrute)) / coutDUnDé;
+                }
+
+                decimal maxNouvValeurEntropie = Convert.ToDecimal((scoreProduitActuel + 2 * valeurProduiteBrute + complexitéAccidentelleActuelle)) / coutDUnDé;
+
+                valeurQualité = meilleurChoixQualite(maxNouvValeurEntropie, minNouvValeurEntropie, valeurInvestissable, scoreProduitActuel, valeurProduiteBrute, complexitéAccidentelleActuelle, coutDUnDé);
+
+                iterations++;
+
+                return valeurQualité;
+            }
+        }
+
+        public uint meilleurChoixQualite(decimal maxNouvValeurEntropie, decimal minNouvValeurEntropie, uint valeurInvestissable, uint scoreProduitActuel, uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint coutDUnDé) {
+
+            bool valide = true;
+            if (Math.Floor(maxNouvValeurEntropie) > Math.Floor(minNouvValeurEntropie)) {
+                decimal nouvValeurEntropie = minNouvValeurEntropie;
+                for (uint i = valeurInvestissable; i > 0; i--){
+                    if (valeurProduiteBrute - i + complexitéAccidentelleActuelle >= i && valide == false){
+                        return complexitéAccidentelleActuelle + valeurInvestissable / 2;
+                    }
+                    if (Math.Floor(maxNouvValeurEntropie) < nouvValeurEntropie && valeurProduiteBrute - i + complexitéAccidentelleActuelle >= i) {
+                        return (i+1);
+                    } else {
+                        nouvValeurEntropie = Convert.ToDecimal((scoreProduitActuel + 2 * valeurProduiteBrute - 4 * i + complexitéAccidentelleActuelle)) / coutDUnDé;
+                    }
+                    if (valeurProduiteBrute - i + complexitéAccidentelleActuelle < i){
+                        valide = false;
+                    }
+                }
+                if (complexitéAccidentelleActuelle > valeurProduiteBrute) {
+                    return 0;
+                } else {
+                    return complexitéAccidentelleActuelle + valeurInvestissable / 2;
+                } 
+            } else {
+                if (complexitéAccidentelleActuelle > valeurProduiteBrute) {
+                    return 0;
+                } else {
+                    return complexitéAccidentelleActuelle + valeurInvestissable / 2;
+                } 
+            }
+        }
+
+        /// <inheritdoc />
+        public IStratégieQualité Fork() => new StratégieClermontGeoffrey();
+    }
+}
