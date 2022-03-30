cd346b1d93bbf44bac46952ed0be0db0f7e55496
AxelB44 <72434670+AxelB44@users.noreply.github.com>
2022-03-30 21:30:50 +0200

    Add files via upload

---
+++ SimulationComplexité/Stratégies/StratégieAxelBORGET.cs
@@ -0,0 +1,23 @@
+﻿using SimulationComplexité.Simulation.Stratégie;
+using SimulationComplexité.Stratégies.Prédéfinies;
+
+namespace SimulationComplexité.Stratégies
+{
+    internal class StratégieAxelBORGET : IStratégieQualité
+    {
+        private static readonly StratégieQuiVaChaptiVaLoin StratégiePrudente = new ();
+
+        /// <inheritdoc />
+        public uint MontantInvestiEnQualité(uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint scoreProduitActuel, ushort coutDUnDé)
+        {
+            if (valeurProduiteBrute < 1) return 0;
+            if (complexitéAccidentelleActuelle > valeurProduiteBrute && valeurProduiteBrute > 1) return valeurProduiteBrute - 2; 
+            if (complexitéAccidentelleActuelle > valeurProduiteBrute) return valeurProduiteBrute;
+            var valeurInvestissableEnProduit = valeurProduiteBrute - complexitéAccidentelleActuelle;
+            return complexitéAccidentelleActuelle + valeurInvestissableEnProduit / 2;
+        }
+
+        /// <inheritdoc />
+        public IStratégieQualité Fork() => new VotreStratégie();
+    }
+}
