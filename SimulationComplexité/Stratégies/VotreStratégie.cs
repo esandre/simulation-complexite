5f52421ecbef1eb7efb87f40a8dd11f1f75dbe78
thomas_mary <tmary@oui.sncf>
2022-03-30 10:46:39 +0200

    algorithme de Thomas MARY et Antoine LEBLANC

--- SimulationComplexité/Stratégies/VotreStratégie.cs
+++ SimulationComplexité/Stratégies/VotreStratégie.cs
@@ -5,39 +5,60 @@ namespace SimulationComplexité.Stratégies
 {
     internal class VotreStratégie : IStratégieQualité
     {
-        private static readonly StratégieQuiVaChaptiVaLoin StratégiePrudente = new ();
-
         /// <inheritdoc />
         public uint MontantInvestiEnQualité(uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint scoreProduitActuel, ushort coutDUnDé)
         {
-            var maximumInvestissable = StratégiePrudente.MontantInvestiEnQualité(
-                valeurProduiteBrute, 
-                complexitéAccidentelleActuelle, 
-                scoreProduitActuel, 
-                coutDUnDé);
+            // BON COURAGE
+            uint nombreDeDes = (complexitéAccidentelleActuelle + scoreProduitActuel) / coutDUnDé;
 
-            var complexitéTotaleInitale = scoreProduitActuel + complexitéAccidentelleActuelle;
-
-            (uint InvestissementQualité, uint NombreDésEntropieProjeté, uint MontantProduit) meilleurInvestissement = 
-                (0, (complexitéTotaleInitale + valeurProduiteBrute * 2) / coutDUnDé, valeurProduiteBrute);
-            
-            for (uint montantInvestiEnQualité = 1; montantInvestiEnQualité <= maximumInvestissable; montantInvestiEnQualité++)
+            if (complexitéAccidentelleActuelle > valeurProduiteBrute)
             {
-                var montantInvestiEnProduit = Convert.ToUInt32(valeurProduiteBrute - montantInvestiEnQualité);
-                var complexitéProduite = montantInvestiEnProduit * 2;
-
-                var complexitéTotaleProjetée =
-                    Convert.ToUInt32(complexitéTotaleInitale + complexitéProduite - montantInvestiEnQualité);
-
-                var nombreDésEntropieProjeté = complexitéTotaleProjetée / coutDUnDé;
-
-                if (nombreDésEntropieProjeté < meilleurInvestissement.NombreDésEntropieProjeté
-                    || nombreDésEntropieProjeté == meilleurInvestissement.NombreDésEntropieProjeté
-                    && montantInvestiEnProduit > meilleurInvestissement.MontantProduit)
-                    meilleurInvestissement = (montantInvestiEnQualité, nombreDésEntropieProjeté, montantInvestiEnProduit);
+                var feature = valeurProduiteBrute % coutDUnDé;
+                //return valeurProduiteBrute > 2 ? valeurProduiteBrute - feature : valeurProduiteBrute; //1046 à peu près
+                return valeurProduiteBrute - feature; //1060 environ et 7.5 val/itération
             }
 
-            return meilleurInvestissement.InvestissementQualité;
+            uint produit;
+            ; //valeur des dés d'entopie actuels
+            uint valeurANePasDepasser = nombreDeDes + 1; // valeur qu'on veut éviter de dépasser pour pas perdre un dé
+            double ratio = valeurANePasDepasser; //valeur à éviter de dépasser pour pas perdre un dé
+
+            uint valeurAInvestir = 0;
+
+            int scoreProduitTotal;
+            int complexiteAccidentelle;
+            int complexiteaccidentelleTotale;
+            uint complexiteTotale;
+
+            for (uint investQualite = 0; investQualite <= valeurProduiteBrute; investQualite++)
+            {
+                //produit = valeur brute - ce qu'on met dans qualité
+                produit = valeurProduiteBrute - investQualite;
+
+                //Score produit total =  produit actuel + produit investi
+                scoreProduitTotal = (int)(scoreProduitActuel + produit);
+
+                //Complexité accidentelle = produi investi - investissement qualité
+                complexiteAccidentelle = (int)(produit - investQualite);
+
+                //complexité accidentelle totale = complexité accidentelle actuelle + complexité accidentelle
+                complexiteaccidentelleTotale = (int)complexitéAccidentelleActuelle + complexiteAccidentelle;
+
+                //Score Total de complexité = Score Prosuit + Complexité accidentelle
+                complexiteTotale = (uint)(scoreProduitTotal + complexiteaccidentelleTotale);
+
+                //Résutat de la division pour voir si on perd un dé ou pas
+                var result = (double)complexiteTotale / coutDUnDé;
+
+
+                // complexiteAccidentelle + complexitéAccidentelleActuelle >= 3 pour un score inférieur mais valeur/itération de 8
+                if (result < valeurANePasDepasser && complexiteAccidentelle + complexitéAccidentelleActuelle >= 2 && result <= ratio)
+                {
+                    valeurAInvestir = investQualite;
+                }
+            }
+
+            return valeurAInvestir != 0 ? valeurAInvestir : valeurProduiteBrute / 2;
         }
 
         /// <inheritdoc />
