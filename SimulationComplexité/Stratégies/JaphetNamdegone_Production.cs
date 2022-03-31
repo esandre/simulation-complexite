22621526f445cc13fb67454374b1f655a05e8741
Enzo Sandré <atelier@enzosandre.fr>
2022-03-31 09:09:47 +0200

    Japhet NAMDEGONE

---
+++ SimulationComplexité/Stratégies/JaphetNamdegone_Production.cs
@@ -0,0 +1,26 @@
+﻿using SimulationComplexité.Simulation.Stratégie;
+
+namespace SimulationComplexité.Stratégies
+{
+    internal class JaphetNamdegone_Production : IStratégieQualité
+    {
+        /// <inheritdoc />
+        public uint MontantInvestiEnQualité(uint valeurProduiteBrute, uint complexitéAccidentelleActuelle, uint scoreProduitActuel, ushort coutDUnDé)
+        {
+            var valeurInvestissableEnProduit = valeurProduiteBrute - complexitéAccidentelleActuelle;
+            var valeur = valeurProduiteBrute % 2;
+            if (valeur == 0) return valeurProduiteBrute / 2;
+            else
+            {
+
+                if (complexitéAccidentelleActuelle > valeurProduiteBrute)
+                    return valeurProduiteBrute - 1  ;
+                return complexitéAccidentelleActuelle + valeurInvestissableEnProduit / 2;
+
+            }
+        }
+
+        /// <inheritdoc />
+        public IStratégieQualité Fork() => new JaphetNamdegone_Production();
+    }
+}
